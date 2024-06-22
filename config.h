6f74b1cec9341754c9f76c40bee7cf095d043639
amirhossein <ahosshams@protonmail.com>
2024-06-22 23:54:10 +0330

    set gruvbox theme

--- config.h
+++ config.h
@@ -12,10 +12,22 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char norm_fg[] = "#D5C4A1";
+static const char norm_bg[] = "#262626";
+static const char norm_border[] = "#665c54";
+static const char sel_fg[] = "#262626";
+static const char sel_bg[] = "#FE8019";
+static const char sel_border[] = "#fbf1c7";
+static const char urg_fg[] = "#fbf1c7";
+static const char urg_bg[] = "#fb4934";
+static const char urg_border[] = "#fb4934";
+static const char title_fg[] = "#98971A";
+static const char title_bg[] = "#3B3835";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg           bg          border                         */
+    [SchemeNorm] =   { norm_fg,   norm_bg,    norm_border }, 		     // unfocused wins
+    [SchemeSel]  =   { sel_fg,    sel_bg,     sel_border },		     // the focused win
 };
 
 /* tagging */
